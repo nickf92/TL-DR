2f60f45e912cdc28d9a72b4365d052da822da365
Nicola Salvatore Francica <nickf92@protonmail.com>
2026-07-21 22:56:29 +0200

    Fix SDK versions to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "it.tldl.app"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "it.tldl.app"
         minSdk = 26
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0.0"
 
